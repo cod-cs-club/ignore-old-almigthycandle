d379af8d29982400d61b06477d77c56269395228
qmatias <mdkotlik@gmail.com>
2022-03-18 17:46:17 -0500

    add setup.sh; rename everything

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+wait() {
+  echo 'When you are done installing, press enter.'
+  read
+}
+
+if ! command -v python &> /dev/null; then
+  echo 'Please install: https://www.python.org/downloads/'
+  wait
+fi
+
+if ! command -v node &> /dev/null; then
+  echo 'Please install NodeJS (do the LTS version): https://nodejs.org/en/download/'
+  wait
+fi
+
+echo 'Please install Github Desktop: https://desktop.github.com/'
+wait
+
+cd ai
+echo 'Setting up python virtual environment...'
+python -m venv --system-site-packages venv
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  source venv/bin/activate
+else
+  .\venv\Scripts\activate
+fi
+
+echo 'Upgrading pip...'
+pip install --upgrade pip
+
+echo 'Installing python dependencies...'
+pip install -r requirements.txt
+
+echo 'Done setting up AI!'
+
+echo 'Setting up nodejs environment...'
+cd ../web
+npm install
