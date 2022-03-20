ed94936cf210afa2ba177013f1fc5dc03cd3d454
D. S <67660328+dimitri-sky@users.noreply.github.com>
2022-03-20 10:17:34 -0500

    AI Imports

---
+++ ai/main.py
@@ -0,0 +1,21 @@
+# YT Video: https://www.youtube.com/watch?v=PuZY9q-aKLw&t=957s&ab_channel=NeuralNine
+# Imports for our Projects
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+
+def print_hi(from_dimitri):
+    print(f'Hi, {from_dimitri}')
+
+
+if __name__ == '__main__':
+    print_hi("What's up CS Club Homies. Let's get freaky.")
