7b55cd47e20a88f08aed36f83d3ecbeff90346fc
Voy7 <89422331+Voy7@users.noreply.github.com>
2022-03-20 14:41:42 -0500

    Website wireframe with basic header and graph.

---
+++ web/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+// Header component.
+function Header() {
+  return (
+    <header>
+      <h1>almigthycandle</h1>
+      <input type="search" placeholder="Search Stock..." />
+    </header>
+  )
+}
+
+export default Header;
--- web/src/index.js
+++ web/src/index.js
@@ -1,17 +1,17 @@
+// Import React.
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Import our custom React components and any other files.
+import Header from './components/Header/Header';
+import Graph from './components/Graph/Graph';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Render the app components.
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <>
+    <Header />
+    <Graph />
+  </>,
+  document.querySelector("#root")
+)
