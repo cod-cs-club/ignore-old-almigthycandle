adcef909ba996ba5d339aac4c9488bb458ba96c6
Zeraxvis <ogradyd98@gmail.com>
2022-03-27 11:56:03 -0500

    Added code for base of model, test, and comilation of data

--- ai/main.py
+++ ai/main.py
@@ -1,38 +1,89 @@
 # YT Video: https://www.youtube.com/watch?v=PuZY9q-aKLw&t=957s&ab_channel=NeuralNine
 # Imports for our Projects
 
+from audioop import minmax
 from tracemalloc import start
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_datareader as web
-import yfinance as yf
+from sklearn.model_selection import PredefinedSplit
+
 import datetime as dt
 
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, scale
 
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 
 #get 1 years worth of data for Apple
-stock_info = yf.Ticker('AAPL').info
-market_price = stock_info['regularMarketPrice']
-previous_close_price = stock_info['regularMarketPreviousClose']
-aaple = yf.Ticker("AAPL")
-prices = aaple.history(period="1y")
+company = 'AAPL'
+start = dt.datetime(2016,1,1)
+end = dt.datetime(2022,1,1)
+data = web.DataReader(company, 'yahoo', start, end)
+prediction_days = 60
+scaler = MinMaxScaler(feature_range=(0,1))
+now = dt.datetime.now()
 
-#graphical representation
-prices['20d'] = prices['Close'].rolling(20).mean() #finds averages for 20 day mark for apple stock
-prices['200d'] = prices['Close'].rolling(200).mean()
-prices[['Close', '20d', '200d']].plot()
-plt.title('Moving Averages Apple')
+#prepare data
 
-#calculate cumulative returns & plotting
-prices['daily_return'] = prices['Close'].pct_change()
-prices['cumulative_return'] = prices['daily_return'].cumsum()
-prices['cumulative_return'].plot()
-plt.title('Cumulative Return Apple')
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# Build Model
+model = tf.keras.Sequential()
+model.add(LSTM(units=(50), return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1)) #Predicition of the next close
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=56)
+
+#load test data
+test_start=dt.datetime(2022,1,1)
+test_end=dt.datetime(2022,3,27)
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
+
+#Make predicition on Test Data
+x_test = []
+for x in range(prediction_days,len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1]), 1)
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+#Plot the test prediction
+
+plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
+plt.plot(predicted_prices, color="green", label=f"Predicted {company} Price")
+plt.title(f"{company} Share Price")
+plt.xlabel('Time')
+plt.ylabel(f'{company} Share Price')
+plt.legend()
+plt.show()
 
 def print_hi(from_dimitri):
     print(f'Hi, {from_dimitri}')
@@ -40,6 +91,6 @@ def print_hi(from_dimitri):
 
 if __name__ == '__main__':
     print_hi("Whats up CS Club!  Howdy, boys.  Lets get Rowdy.")
-    print(prices)
+    
     
 
