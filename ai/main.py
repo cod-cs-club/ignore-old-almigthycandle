2e7c81425dce689755cf68683be4e03d5e607032
Zeraxvis <ogradyd98@gmail.com>
2022-03-27 12:06:13 -0500

    Updated test_end to now

--- ai/main.py
+++ ai/main.py
@@ -55,7 +55,7 @@ model.fit(x_train, y_train, epochs=25, batch_size=56)
 
 #load test data
 test_start=dt.datetime(2022,1,1)
-test_end=dt.datetime(2022,3,27)
+test_end=dt.datetime.now()
 
 test_data = web.DataReader(company, 'yahoo', test_start, test_end)
 actual_prices = test_data['Close'].values
