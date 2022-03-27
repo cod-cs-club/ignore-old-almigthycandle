ac9d4de364342ce0571d36af613d9908b60a7808
Zeraxvis <ogradyd98@gmail.com>
2022-03-26 19:31:39 -0500

    Added yfinance api and included print of data

--- ai/main.py
+++ ai/main.py
@@ -1,10 +1,12 @@
 # YT Video: https://www.youtube.com/watch?v=PuZY9q-aKLw&t=957s&ab_channel=NeuralNine
 # Imports for our Projects
 
+from tracemalloc import start
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_datareader as web
+import yfinance as yf
 import datetime as dt
 
 from sklearn.preprocessing import MinMaxScaler
@@ -13,6 +15,11 @@ import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 
+df_yahoo = yf.download('AAPL',
+start='2022-01-01',
+end='2022-03-26',
+progress=False, auto_adjust=True)
+
 
 def print_hi(from_dimitri):
     print(f'Hi, {from_dimitri}')
@@ -20,3 +27,4 @@ def print_hi(from_dimitri):
 
 if __name__ == '__main__':
     print_hi("Whats up CS Club!  Howdy, boys.  Lets get Rowdy.")
+    print_hi(df_yahoo)
