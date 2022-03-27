6c172666184996cdf6fcf71915ef51804c127803
Zeraxvis <ogradyd98@gmail.com>
2022-03-26 20:21:17 -0500

    Added cumulative data

--- ai/main.py
+++ ai/main.py
@@ -15,15 +15,24 @@ import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 
+#get 1 years worth of data for Apple
 stock_info = yf.Ticker('AAPL').info
 market_price = stock_info['regularMarketPrice']
 previous_close_price = stock_info['regularMarketPreviousClose']
 aaple = yf.Ticker("AAPL")
 prices = aaple.history(period="1y")
+
+#graphical representation
 prices['20d'] = prices['Close'].rolling(20).mean() #finds averages for 20 day mark for apple stock
 prices['200d'] = prices['Close'].rolling(200).mean()
 prices[['Close', '20d', '200d']].plot()
 plt.title('Moving Averages Apple')
+
+#calculate cumulative returns & plotting
+prices['daily_return'] = prices['Close'].pct_change()
+prices['cumulative_return'] = prices['daily_return'].cumsum()
+prices['cumulative_return'].plot()
+plt.title('Cumulative Return Apple')
 
 def print_hi(from_dimitri):
     print(f'Hi, {from_dimitri}')
@@ -32,4 +41,5 @@ def print_hi(from_dimitri):
 if __name__ == '__main__':
     print_hi("Whats up CS Club!  Howdy, boys.  Lets get Rowdy.")
     print(prices)
+    
 
