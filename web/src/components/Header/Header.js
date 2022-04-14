57bb3f88382c1081c31f010fdaa95b968b2464c6
Voy7 <89422331+Voy7@users.noreply.github.com>
2022-04-14 17:51:03 -0500

    Updated header navigation

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <header>
             <h1>almightycandle</h1>
             <ul>
-                <li><a href="#">Home</a></li>
+                <li><a href="#" className="selected">Home</a></li>
                 <li><a href="#">What is ALCA?</a></li>
                 <li><a href="#">Meet The Team</a></li>
                 <li><a href="#">Discord</a></li>
